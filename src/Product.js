be2ea9326e9fc26317dc5a912dc5f43d333af1c4
meher <gmkishor05@gmail.com>
2023-12-07 18:27:11 +0530

    newupdate

--- src/Product.js
+++ src/Product.js
@@ -3,14 +3,12 @@ import React from 'react'
 export default function Product(props) {
   return (
     <>
-    
     <div className="Products" >
     <img src={props.url}/>
     <h2>{props.name}</h2>
-
-    
      <div><p> {props.price}</p></div>
-  
+  <div> 
+  <button> select</button></div>
       
     </div>
      
